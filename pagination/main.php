92fa2a608ec3a702f341764de2cec596ec02672c
Rocco Tripaldi <tripaldir@gmail.com>
2018-12-06 09:49:24 -0500

    fix pagination on small screens

--- pagination/main.php
+++ pagination/main.php
@@ -1,6 +1,6 @@
 <div class="dammed-card dammed-pagination" data-type="rocco">
-	<div class="dammed-content">
-		<p>This is the end of the page.<br />Where would you like to go next?</p>
-		<?php the_posts_pagination( array( 'mid_size' => 2 ) ); ?>
-	</div>
+    <div class="dammed-content">
+        <p>This is the end of the page.<br />Where would you like to go next?</p>
+		<?php the_posts_pagination( array( 'mid_size' => 1, 'prev_text' => '&ltrif;', 'next_text' => '&rtrif;' ) ); ?>
+    </div>
 </div>
